348bc8cd11a6e81e43ff1443c992c4d15f7acb4b
ValeriaPonomarenko <PVaLeriiaA@mail.ru>
2023-11-02 21:59:17 +0500

    Закончила всю работу

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,19 @@
-<template></template>
+<template>
+    <div :style="customStyles"></div>
+</template>
 
 <script>
 export default {
   name: "App",
+  computed: {
+    customStyles(){
+      return{
+        width: "40px",
+        height: "40px",
+        backgroundColor: "red",
+      }
+    }
+  }
 };
 </script>
 
